2fb5ad34ee4af1b7b5b43a8ecdc84ac23042c739
kipariss1 <kpassudin@gmail.com>
2024-11-25 01:48:38 +0100

    added basic authentication done, TODO: implement e-mail sending

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, EmailStr, model_validator, field_validator
+from pydantic import BaseModel, Field, EmailStr, field_validator
 import re
 from datetime import datetime
 
